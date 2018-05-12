675e54b56437d833d736b374327ef28fd5ffc024
JayEss__ <jayess.choi@gmail.com>
2018-05-12 18:59:25 +0900

    fixed typo

--- matplotlib_prac_1.py
+++ matplotlib_prac_1.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 데이터 준비
-x = np.arange(0, 2 * np.pi, 0.01)  # 0~2PI 범위에서 0.1 간격으로 생성
+x = np.arange(0, 2 * np.pi, 0.01)  # 0~2PI 범위에서 0.01 간격으로 생성
 y1 = np.sin(x)
 y2 = np.cos(x)
 
