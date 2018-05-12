fb60bab0e9f4ae3d6842f30e18c0d59b475b685b
JayEss__ <jayess.choi@gmail.com>
2018-05-12 18:35:35 +0900

    180512_1835

---
+++ matplotlib_prac_1.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 데이터 준비
+x = np.arange(0, 2 * np.pi, 0.01)  # 0~2PI 범위에서 0.1 간격으로 생성
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+# 그래프 그리기
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, label="cos", linestyle="--")
+plt.xlabel("x") # x축 이름
+plt.ylabel("y") # y축 이름
+plt.title("sin & cos") # 제목
+plt.legend()
+plt.show()
